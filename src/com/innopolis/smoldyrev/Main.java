f39e9ba6638087c80d122348734e78cef4c86c7f
Egor Smoldyrev <esmoldyrev@gmail.com>
2017-02-08 17:52:30 +0300

    Перед потоками/Добавил цикл по параметрам

--- src/com/innopolis/smoldyrev/Main.java
+++ src/com/innopolis/smoldyrev/Main.java
@@ -11,24 +11,27 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-//        ArrayList<ResourceReader> resources = new ArrayList<>();
-//        for (String filePath:
-//             args) {
-//            resources.add(new ResourceReader(filePath));
-//        }
-        String filePath = args[0];
-        ResourceReader reader;
 
         try {
-
-            reader = new ResourceReader(filePath);
-            reader.sendTextToCounter();
-            reader.closeReader();
+//            ArrayList<ResourceReader> resources = new ArrayList<>();
+            for (String filePath:
+                    args) {
+                ResourceReader resource = new ResourceReader(filePath);
+//                resources.add(resource);
+                resource.sendTextToCounter();
+                resource.closeReader();
+            }
+//
+//            for (ResourceReader reader:
+//                 resources) {
+//                reader.sendTextToCounter();
+//                reader.closeReader();
+//            }
 
         } catch (FileNotFoundException e) {
             System.out.println("Файл: "+filePath+" не найден");
         } catch (MalformedURLException e) {
-            System.out.println("Ссылка: "+filePath+" имеет неправльный формат");
+            System.out.println("Ссылка: "+filePath+" имеет неправильный формат");
         } catch (IOException e) {
             System.out.println("Проблема с получением файла");
         } catch (Exception e) {
